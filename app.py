31ea07478439523c3a431163d219f1abae81801d
NicollasSilvaG <nicollasaprv8@gmail.com>
2025-02-24 19:59:22 -0300

    Versão 1: Adiciona função básica

---
+++ app.py
@@ -0,0 +1,8 @@
+def bissexto(ano):
+    if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+        print("O ano é bissexto")
+    else:
+        print("O ano não é bissexto")
+
+ano = int (input("Digite um ano: "))
+bissexto(ano)
