70ecc9e901b7db52c7a032151c04d71256041b41
NicollasSilvaG <nicollasaprv8@gmail.com>
2025-02-24 20:04:40 -0300

    Versão 3: Permite múltiplas entradas e adiciona opção de saída

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 def bissexto(ano):
     return ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0)
 
-ano = int(input("Digite um ano: "))
-if bissexto(ano):
-    print("O ano é bissexto.")
-else:
-    print("O ano não é bissexto.")
+while True:
+    ano = input("Digite um ano (ou 'sair' para encerrar): ")
+    if ano.lower() == "sair":
+        break
+    elif ano.isdigit():
+        if bissexto(int(ano)):
+            print(f"{ano} é um ano bissexto.")
+        else:
+            print(f"{ano} não é um ano bissexto.")
+    else:
+        print("Entrada inválida. Digite um ano válido ou 'sair'.")
