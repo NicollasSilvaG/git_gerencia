58310ef5bf352219cd17d414818716f822d1e68e
NicollasSilvaG <nicollasaprv8@gmail.com>
2025-02-24 20:03:42 -0300

    Versão 2: Retorna True/False ao invés de imprimir diretamente

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def bissexto(ano):
-    if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
-        print("O ano é bissexto")
-    else:
-        print("O ano não é bissexto")
+    return ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0)
 
-ano = int (input("Digite um ano: "))
-bissexto(ano)
+ano = int(input("Digite um ano: "))
+if bissexto(ano):
+    print("O ano é bissexto.")
+else:
+    print("O ano não é bissexto.")
